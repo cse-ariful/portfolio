1535c7913299af3fa83bd9a93827d2cb0bc572f7
Ariful Jannat Arif <cse.ariful@gmail.com>
2022-09-27 16:04:47 +0600

    update app js

--- app.js
+++ app.js
@@ -4,9 +4,6 @@
             document.getElementById(button.dataset.id).scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
             document.querySelector(".active-btn").classList.remove("active-btn");
             this.classList.add("active-btn");
-            document.querySelector(".active").classList.remove("active");
-            document.getElementById(button.dataset.id).classList.add("active");
-
         })
     });
     document.querySelector(".theme-btn").addEventListener("click", () => {
