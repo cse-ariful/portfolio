12573fe4624185e028315c2ddb50360bf0aab78a
Ariful Jannat Arif <cse.ariful@gmail.com>
2022-09-27 15:55:05 +0600

    changes

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
     [...document.querySelectorAll(".control")].forEach(button => {
         button.addEventListener("click", function () {
             document.getElementById(button.dataset.id).scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
-            // document.querySelector(".active-btn").classList.remove("active-btn");
-            // this.classList.add("active-btn");
-            // document.querySelector(".active").classList.remove("active");
-            // document.getElementById(button.dataset.id).classList.add("active");
+            document.querySelector(".active-btn").classList.remove("active-btn");
+            this.classList.add("active-btn");
+            document.querySelector(".active").classList.remove("active");
+            document.getElementById(button.dataset.id).classList.add("active");
 
         })
     });
